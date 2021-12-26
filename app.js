c42950f7dc846eb425a11db4eec9a0271ede16a7
Stuckens <kelian2009@hotmail.fr>
2021-12-26 15:51:27 +0100

    salut

--- app.js
+++ app.js
@@ -1,17 +1,63 @@
-const url ='https://xora123.github.io/Kélian.json';
+const url = "https://xora123.github.io/Kélian.json"; // url de mon fichier JSON
 
-async function getItems(){
-    const responce = await fetch(url);
-    const data = await responce.json();
-    for(let i=0; i<25; i++){
-        var c = document.getElementById('questions').textContent = data.quizz.fr.débutant[i].question;
-        var b = document.getElementById('propostions').textContent = data.quizz.fr.débutant[i].propositions;
-        var newDiv = document.createElement('div');
-    // console.log(data.quizz.fr.débutant[i].propositions);
-    // console.log(data.quizz.fr.débutant[i].réponse)
-    // console.log(data.quizz.fr.débutant[i].anecdote)
+var Emptytab = []; // Création d'un tableau vide pour mettre mes data dedans 
 
-    }
+// Fonction pour prendre les data de mon fichier JSON
+async function getData() {
+  const responce = await fetch(url);
+  const data = await responce.json();
+
+  return data.quizz.fr.débutant;
+}
+const copyMyData = async (data) => {
+  const FillTab = await getData(data.quizz.fr.débutant);
+  Emptytab.push(FillTab);
+
+  return Emptytab;
+};
+copyMyData();
+
+
+
+const container = document.getElementById("container");
+
+function createQuestion() {
+  const NewDiv = document.getElementById("question");
+
+  const NewText = document.createTextNode("Question");
+
+  NewDiv.appendChild(NewText);
+
+  return NewDiv;
 }
 
-getItems()
+function tri(array) {
+    var a;
+    var b;
+    var temp;
+
+    if (Array.isArray(array) === true) { // Si la fonction = true c'est un array , sinon non
+      if (array.length == 0 || array.length == 1)  // Si la taille du tableau est de 0 alors il n'y a pas de Tab, et si elle est de 1 alors pas besoin de trier
+        return;
+
+      for (a = array.length; a != 0; a--) { // Ici je fais ma fonction de tri, j'assigne à A ma tab.length, et je décremente jusqu'a 0
+        b = Math.floor(Math.random() * array.length); // Ici j'assigne a B un nombre entre 0 et la taille du tableau
+
+        temp = array[b]; // Je mets dans ma variable temporaire la valeur de la case b
+        array[b] = array[a]; // Je mets dans la case b la variable de la case a
+        array[a] = temp; // Je mets dans la case A la variable temp
+
+      }
+    }
+    else{
+        alert("It's not a tab BOIZ") // Alert pour pas un tab
+    }
+
+    console.log(array)
+    return array;
+  }
+
+  console.log(Emptytab)
+  console.log(Emptytab[0])
+  console.log("la taille est de" + Emptytab[0].length)
+ tri(Emptytab)
