b85d79e0c1c0ce028b5d552ef2c90a3d116a0d22
Stuckens <kelian2009@hotmail.fr>
2021-12-26 17:22:06 +0100

    salut

--- app.js
+++ app.js
@@ -7,17 +7,15 @@ async function getData() {
   const responce = await fetch(url);
   const data = await responce.json();
 
+  tri(data.quizz.fr.débutant)
   return data.quizz.fr.débutant;
 }
 const copyMyData = async (data) => {
   const FillTab = await getData(data);
-  Emptytab.push(FillTab);
+  Emptytab.push(...FillTab);
 
   return Emptytab;
 };
-copyMyData();
-
-
 
 const container = document.getElementById("container");
 
@@ -28,7 +26,9 @@ function createQuestion() {
 
   NewDiv.appendChild(NewText);
 
+  console.log(NewDiv)
   return NewDiv;
+
 }
 
 function tri(array) {
@@ -40,7 +40,7 @@ function tri(array) {
       if (array.length == 0 || array.length == 1)  // Si la taille du tableau est de 0 alors il n'y a pas de Tab, et si elle est de 1 alors pas besoin de trier
         return;
 
-      for (a = array.length; a != 0; a--) { // Ici je fais ma fonction de tri, j'assigne à A ma tab.length, et je décremente jusqu'a 0
+      for (a = array.length - 1 ; a != 0; a--) { // Ici je fais ma fonction de tri, j'assigne à A ma tab.length, et je décremente jusqu'a 0
         b = Math.floor(Math.random() * array.length); // Ici j'assigne a B un nombre entre 0 et la taille du tableau
 
         temp = array[b]; // Je mets dans ma variable temporaire la valeur de la case b
@@ -53,11 +53,10 @@ function tri(array) {
         alert("It's not a tab BOIZ") // Alert pour pas un tab
     }
 
-    console.log(array)
-    return array;
-  }
 
+    return array;
+  };
+
+  copyMyData();
   console.log(Emptytab)
   console.log(Emptytab[0])
-  console.log("la taille est de" + Emptytab[0].length)
- tri(Emptytab)
